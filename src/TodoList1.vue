684b1f7255e612feba628947fd5caa6f661e3ecf
lcfhn168 <lcfhn168@163.com>
2019-06-23 21:42:55 +0800

    1

---
+++ src/TodoList1.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png">
+    <HelloWorld/> -->
+      <input v-model="inputValue"/>
+      <button @click="handlesubmit">提交</button>
+      <ul>
+      <todo-item v-for="(item, index) of list" :key="index" :content="item"
+      :index="index"
+      @delete="handledelete"
+      ></todo-item>
+      </ul>
+  </div>
+</template>
+
+<script>
+
+// import HelloWorld from './components/HelloWorld'
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+
+import  TodoItem from './components/TodoItem'
+export default{
+  // data:function () {
+  //      return { inputValue:''}
+  // }
+
+  components:{
+    'todo-item':TodoItem
+  },
+   data () {
+       return { 
+         inputValue:'',
+         list:[]
+       
+       }
+  },
+  methods:{
+    handlesubmit(){
+     this.list.push(this.inputValue)
+     this.inputValue=''
+    },
+    handledelete (index){
+      this.list.splice(index,1);
+    }
+  }
+
+}
+
+
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li @click="handledelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+ props :['content','index'],
+ methods:{
+   handledelete (){
+    alert(this.index);
+    this.$emit('delete', this.index)   
+   }
+ }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
